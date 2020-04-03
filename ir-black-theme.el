f61be9f9d2b516d4c36b03b6066f1908f7d59871
Filip Miletic <filip.miletic@me.com>
2020-04-03 15:31:13 +0200

    updt

--- ir-black-theme.el
+++ ir-black-theme.el
@@ -48,63 +48,63 @@
 
 (let ((*background-color*   "#101010")
       (*brown*              "#E64")
-      (*comments*           "#3fdf1f")
+      (*comments*           "#7C7C7C")
       (*constant*           "#99CC99")
-      (*current-line*       "#222222")
+      (*current-line*       "#151515")
       (*cursor-underscore*  "#FFFAAA")
-      (*keywords*           "#aaaaaa") ;;96CBFE
+      (*keywords*           "#96CBFE")
       (*light-purple*       "#FFCCFF")
       (*line-number*        "#3D3D3D")
       (*method-declaration* "#FFD2A7")
-      (*mode-line-bg*       "#222222")
+      (*mode-line-bg*       "#202020")
       (*mode-line-fg*       "#CCCCCC")
-      (*normal*             "#ffffff") ;;959bf4
+      (*normal*             "#F6F3E8")
       (*number*             "#FF73FD")
       (*operators*          "#FFFFB6")
       (*red*                "#FF6C60")
       (*red-light*          "#FFB6B0")
       (*regexp*             "#E9C")
       (*regexp-alternate*   "#FF0")
-      (*regexp-alternate-2* "#EFCC29")
+      (*regexp-alternate-2* "#B18A3D")
       (*search-selection*   "#2F2F00")
-      (*string*             "#b8FF60")
-      (*sstring*            "#5555ff")
-      (*string-inner*       "#50c0c0")
+      (*string*             "#A8FF60")
+      (*string-inner*       "#30c0b0")
       (*variable*           "#C6C5FE")
-      (*visual-selection*   "#262D81"))
+      (*visual-selection*   "#262D51"))
 
   (custom-theme-set-faces
    'ir-black
 
    `(bold ((t (:bold t))))
    `(button ((t (:foreground, *keywords* :underline t))))
-   `(default ((t (:background, *background-color* :foreground, *keywords*))))
+   `(default ((t (:background, *background-color* :foreground, *normal*))))
    `(escape-glyph ((t (:foreground, *string-inner*))))
    `(header-line ((t (:background, *mode-line-bg* :foreground, *normal*)))) ;; info header
    `(highlight ((t (:background, *current-line*))))
    `(highlight-face ((t (:background, *current-line*))))
    `(hl-line ((t (:background, *current-line* :underline t))))
    `(info-xref ((t (:foreground, *keywords* :underline t))))
-   `(region ((t (:background, *visual-selection* :foreground, "white"))))
+   `(region ((t (:background, *visual-selection*))))
    `(underline ((nil (:underline t))))
 
    ;; font-lock
    `(font-lock-builtin-face ((t (:foreground, *operators*))))
-   `(font-lock-comment-delimiter-face ((t (:foreground, *comments*))))
-   `(font-lock-comment-face ((t (:foreground, *comments*))))
-   `(font-lock-constant-face ((t (:foreground "#959bf4"))))
-   `(font-lock-doc-face ((t (:foreground, *comments*))))
-   `(font-lock-doc-string-face ((t (:foreground, *comments*))))
-   `(font-lock-function-name-face ((t (:foreground, *constant*))))
-   `(font-lock-keyword-face ((t (:foreground, *normal*))))
+   `(font-lock-comment-delimiter-face ((t (:foreground, *string-inner*))))
+   `(font-lock-comment-face ((t (:foreground, *string-inner*))))
+   `(font-lock-constant-face ((t (:foreground, *constant*))))
+   `(font-lock-doc-face ((t (:foreground, *string*))))
+   `(font-lock-doc-string-face ((t (:foreground, *string*))))
+   `(font-lock-function-name-face ((t (:foreground, *method-declaration*))))
+   `(font-lock-keyword-face ((t (:foreground, *keywords*))))
    `(font-lock-negation-char-face ((t (:foreground, *red*))))
+   `(font-lock-number-face ((t (:foreground, *number*))))
    `(highlight-numbers-number ((t (:foreground, *number*))))
    `(font-lock-preprocessor-face ((t (:foreground, *keywords*))))
    `(font-lock-reference-face ((t (:foreground, *constant*))))
    `(font-lock-regexp-grouping-backslash ((t (:foreground, *regexp*))))
    `(font-lock-regexp-grouping-construct ((t (:foreground, *regexp*))))
    `(font-lock-string-face ((t (:foreground, *string*))))
-   `(font-lock-type-face ((t (:foreground, *string-inner*))))
+   `(font-lock-type-face ((t (:foreground, *operators*))))
    `(font-lock-variable-name-face ((t (:foreground, *variable*))))
    `(font-lock-warning-face ((t (:foreground, *red*))))
 
@@ -114,14 +114,14 @@
    `(minibuffer-prompt ((t (:foreground, *variable*))))
    `(mode-line ((t (:background, *mode-line-bg* :foreground, *mode-line-fg*))))
    `(mode-line-inactive ((t (:background, *mode-line-bg* :foreground, *background-color*))))
-   `(mode-line-buffer-id ((t (:foreground, *string-inner*))))
-   `(cursor ((t (:background "#ff0068")))) ;; or green
-   `(text-cursor ((t (:background, *background-color*))))
+   `(mode-line-buffer-id ((t (:foreground, *string-inner* :weight bold))))
+   `(cursor ((t (:background "#ff0070" :foreground "#000000"))))
+   `(text-cursor ((t (:background, *cursor-underscore*))))
    `(vertical-border ((t (:foreground, *background-color*)))) ;; between splits
 
    ;; show-paren
    `(show-paren-mismatch ((t (:background, *red* :foreground, *normal* :weight bold))))
-   `(show-paren-match ((t (:background, *regexp-alternate-2* :foreground, *current-line* :weight bold))))
+   `(show-paren-match ((t (:background, *brown* :foreground, *background-color* :weight bold))))
 
    ;; search
    `(isearch ((t (:background, *regexp-alternate* :foreground, *search-selection*))))
@@ -141,17 +141,6 @@
    `(org-clock-overlay ((t (:foreground, *mode-line-bg* :background, *string*))))
 
    ;; starter kit
-
-    '(company-tooltip ((t (:foreground "black" :background "grey"))))
- '(company-tooltip-common ((t (:foreground "black" :background "lightgrey"))))
- '(company-tooltip-common-selection ((t (:foreground "white" :background "steelblue"))))
- '(company-tooltip-selection ((t (:foreground "black" :background "steelblue"))))
- '(company-preview-common ((t (:background nil :foreground "lightgrey" :underline t))))
- '(company-scrollbar-fg ((t (:background "orange"))))
- '(company-scrollbar-bg ((t (:background "gray40"))))
- '(company-template-field ((t (:background nil :foreground "yellow"))))
- '(company-tooltip-annotation ((t (:background nil :foreground "dimgray"))))
-   
    `(esk-paren-face ((t (:foreground, *string-inner*))))))
 
 ;;;###autoload
